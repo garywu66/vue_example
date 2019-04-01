7b6606f736a436febbf1111a7f1fe203cd1ba4ea
gary <wgary66329@gmail.com>
2019-04-01 15:47:00 +0800

    i18n 只留下中文繁體

--- src/lang/index.js
+++ src/lang/index.js
@@ -1,34 +1,21 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 import Cookies from 'js-cookie'
-import elementEnLocale from 'element-ui/lib/locale/lang/en' // element-ui lang
-import elementZhLocale from 'element-ui/lib/locale/lang/zh-CN'// element-ui lang
-import elementEsLocale from 'element-ui/lib/locale/lang/es'// element-ui lang
-import enLocale from './en'
-import zhLocale from './zh'
-import esLocale from './es'
+import elementTwLocale from 'element-ui/lib/locale/lang/zh-TW'// element-ui lang
+import twLocale from './tw'
 
 Vue.use(VueI18n)
 
 const messages = {
-  en: {
-    ...enLocale,
-    ...elementEnLocale
-  },
-  zh: {
-    ...zhLocale,
-    ...elementZhLocale
-  },
-  es: {
-    ...esLocale,
-    ...elementEsLocale
+  tw: {
+    ...twLocale,
+    ...elementTwLocale
   }
 }
 
 const i18n = new VueI18n({
   // set locale
-  // options: en | zh | es
-  locale: Cookies.get('language') || 'en',
+  locale: Cookies.get('language') || 'tw',
   // set locale messages
   messages
 })
